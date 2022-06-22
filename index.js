850eef1dbb0fe7cc39442de39ee977849c783104
Nuogz <zheung@vip.qq.com>
2022-06-22 16:15:45 +0800

    1.4.3 export Hades and Poseidon now

--- index.js
+++ index.js
@@ -10,35 +10,39 @@ E = E.filter(env => env.trim()).reduce((e, env) => {
 	return e;
 }, {});
 
+const isExportAll = '*' in E;
 
 
-export const dirPackage = ('*' in E || 'dir' in E) ? (await import('./lib/dir.js')).dirPackage : undefined;
-
-
-
-export const PKG = ('*' in E || 'pkg' in E || 'package' in E) ? (await import('./lib/package.js')).default : undefined;
+export const dirPackage = (isExportAll || 'dir' in E) ? (await import('./lib/dir.js')).dirPackage : undefined;
 
 
 
-export const O = ('*' in E || 'cmd' in E || 'command' in E) ? (await import('./lib/command.js')).default : undefined;
+export const PKG = (isExportAll || 'pkg' in E || 'package' in E) ? (await import('./lib/package.js')).default : undefined;
 
 
 
-export const C = ('*' in E || 'cfg' in E || 'conf' in E || 'config' in E) ? (await import('./lib/config.js')).default : undefined;
+export const O = (isExportAll || 'cmd' in E || 'command' in E) ? (await import('./lib/command.js')).default : undefined;
 
 
 
-export const G = ('*' in E || 'log' in E || 'logger' in E) ? (await import('./lib/log.js')).default : undefined;
+export const C = (isExportAll || 'cfg' in E || 'conf' in E || 'config' in E) ? (await import('./lib/config.js')).default : undefined;
 
 
 
-export const Moment = ('*' in E || 'moment' in E) ? (await import('./lib/moment.js')).default : undefined;
+export const G = (isExportAll || 'log' in E || 'logger' in E) ? (await import('./lib/log.js')).default : undefined;
 
 
 
-export const copyJSON = ('*' in E || 'tool' in E) ? (await import('./lib/tool.js')).copyJSON : undefined;
-export const readPackage = ('*' in E || 'tool' in E) ? (await import('./lib/package.js')).readPackage : undefined;
+export const Moment = (isExportAll || 'moment' in E) ? (await import('./lib/moment.js')).default : undefined;
+
+
+export const Poseidon = (isExportAll || 'Poseidon' in E) ? (await import('@nuogz/poseidon')).default : undefined;
+export const Hades = (isExportAll || 'Hades' in E) ? (await import('@nuogz/hades')).default : undefined;
+
+
+export const copyJSON = (isExportAll || 'tool' in E) ? (await import('./lib/tool.js')).copyJSON : undefined;
+export const readPackage = (isExportAll || 'tool' in E) ? (await import('./lib/package.js')).readPackage : undefined;
 
 
 
-if('*' in E || 'proc' in E || 'process' in E) { await import('./lib/process.js'); }
+if(isExportAll || 'proc' in E || 'process' in E) { await import('./lib/process.js'); }
